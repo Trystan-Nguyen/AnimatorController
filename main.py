c20f909543b022726c8c351923d0789b9f3cf712
Trystan Nguyen <trystan.t.nguyen@gmail.com>
2024-01-24 15:41:14 -0800

    Testing check

--- main.py
+++ main.py
@@ -2,19 +2,30 @@ from main_controller import blendshape_controller
 import cv2, numpy, pickle, time, pprint
 
 if __name__ == '__main__':
-	ctr = blendshape_controller(0)
+    ctr = blendshape_controller(0)
 
-	#ctr.run_body_tracking()
-	ctr.run_face_tracking()
-	#ctr.run_hand_tracking()
-	input("Enter to print data")
-	#pprint.pprint(ctr.get_body_detections())
-	#print('\n\n\n---------------------------------------------------------------------\n\n\n')
-	#pprint.pprint(ctr.get_hand_detections())
-	#print('\n\n\n---------------------------------------------------------------------\n\n\n')
-	print(ctr.get_face_detections())
-	#print('\n\n\n---------------------------------------------------------------------\n\n\n')
-	
-	input("Enter to quit:")
-	print(ctr.get_face_detections())
-	ctr.terminate()
+    #ctr.run_body_tracking()
+    ctr.run_face_tracking()
+    #ctr.run_hand_tracking()
+
+    size = 0
+    try:
+        while True:
+            temp = ctr.get_face_detections()
+            size = max(size, len(temp))
+            print(f'{size}', end='\r')
+    except KeyboardInterrupt:
+        pass
+    print(f'Max Size: {size}')
+
+    #input("Enter to print data")
+    #pprint.pprint(ctr.get_body_detections())
+    #print('\n\n\n---------------------------------------------------------------------\n\n\n')
+    #pprint.pprint(ctr.get_hand_detections())
+    #print('\n\n\n---------------------------------------------------------------------\n\n\n')
+    #print(ctr.get_face_detections())
+    #print('\n\n\n---------------------------------------------------------------------\n\n\n')
+
+    input("Enter to quit:")
+    #print(ctr.get_face_detections())
+    ctr.terminate()
